1592899635cf8309ae51e3c051e212dc1afbd5ad
CHAN125 <buhuibuhui0.0@gmail.com>
2024-05-02 13:57:14 +0800

    added new line

--- CustomerInterface/cart.py
+++ CustomerInterface/cart.py
@@ -1,1 +1,29 @@
-from flask import Flask
+from flask import Flask, request	
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Cart Backend'  
+
+shopping_cart = [] 
+
+@app.route('/cart/add', methods=['GET'])
+def cart_add():
+  
+    item_name = request.args.get('name', 'Item')
+    item_price = request.args.get('price', '0.00')
+    
+    item = {'name': item_name, 'price': item_price}
+    shopping_cart.append(item) 
+    
+    return f"Added {item_name} with {item_price} to the cart."
+
+shopping_cart = []
+
+@app.route('/cart', methods=['GET'])
+def cart_get():
+    cart_items = ", ".join([f"{item['name']} (RM{item['price']})" for item in shopping_cart])
+    
+    return f"Cart contains: {cart_items}"
+
