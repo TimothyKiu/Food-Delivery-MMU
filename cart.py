7102843f09f7df95a5e4ff57a0cfdc3f1cd93b58
CHAN125 <buhuibuhui0.0@gmail.com>
2024-05-14 03:39:03 +0800

    try connect with db

--- cart.py
+++ cart.py
@@ -1,32 +1,56 @@
 from flask import Flask, request, session
+import mysql.connector
+
+#NOTE: STOP RUNNING PYTHON WHENEVER YOU WANNA ALTER SQL TABLES
+db = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    passwd='Chan@125',
+    database='customer'
+)
+
+mycursor = db.cursor(buffered=True)
 
 app = Flask(__name__)
-app.secret_key = 'mysecretkey'
 
 
-@app.route('/addtocart', methods=['POST'])
-def addtocart():
-    item_name = request.form['item_name']
-    item_price = float(request.form['item_price'])
-    if 'cart' not in session:
-        session['cart'] = []
-    session['cart'].append({'name': item_name, 'price': item_price})
-    return 'Item added to cart'
+@app.route('/cart.py/remarks', methods=['POST'])
+def remarks():
+    if request.method == 'POST':
 
-@app.route('/cart')
-def view_cart():
-    if 'cart' not in session:
-        return 'Cart is empty'
-    return ''.join([f'<li>{item["name"]} - RM{item["price"]}</li>' for item in session['cart']])
+        remark = request.form.get('remark')
 
-@app.route('/clear_cart')
-def clear_cart():
-    session.pop('cart', None)
-    return 'Cart cleared'
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO customer (customer_name) VALUES (%s)", (remark))
+        db.commit()
+    return 'success' 
 
-# @app.route('/')
-# def home():
-#     return 'Cart Backend'  
+
+@app.route('/')
+def home():
+    return 'Cart Backend'  
+
+
+# @app.route('/addtocart', methods=['POST'])
+# def addtocart():
+#     item_name = request.form['item_name']
+#     item_price = float(request.form['item_price'])
+#     if 'cart' not in session:
+#         session['cart'] = []
+#     session['cart'].append({'name': item_name, 'price': item_price})
+#     return 'Item added to cart'
+
+# @app.route('/cart')
+# def view_cart():
+#     if 'cart' not in session:
+#         return 'Cart is empty'
+#     return ''.join([f'<li>{item["name"]} - RM{item["price"]}</li>' for item in session['cart']])
+
+# @app.route('/clear_cart')
+# def clear_cart():
+#     session.pop('cart', None)
+#     return 'Cart cleared'
+
 
 # shopping_cart = [] 
 
@@ -48,3 +72,4 @@ def clear_cart():
     
 #     return f"Cart contains: {cart_items}"
 
+app.run(debug=False)
