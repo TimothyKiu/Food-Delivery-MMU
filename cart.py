a103df1a08719e09c1683babbcb05d87aef258e0
CHAN125 <buhuibuhui0.0@gmail.com>
2024-05-16 14:39:05 +0800

    cart can display, can added remarks

--- cart.py
+++ cart.py
@@ -1,23 +1,28 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template,redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])  # Specify methods for each route
+@app.route('/', methods=['GET']) 
 def menu():
-    return render_template('menu.html')
+    with open("remarks.txt", "r") as file:
+        remarks = file.read()
+    return render_template('menu.html', remarks=remarks)
+
+@app.route('/', methods=['GET']) 
+def home():
+    return render_template('home.html')
 
 
-@app.route('/add_to_cart', methods=['POST'])  # Specify methods for each route
-def addtocart():
+@app.route('/add_to_cart', methods=['POST']) 
+def add_to_cart():
     remarks = request.form.get('remarks')
 
-    # Append remarks to text file (assuming write permissions)
     with open("remarks.txt", "a") as file:
         file.write(remarks + "\n")
 
+    # return render_template('menu.html', remarks=remarks)
     return "successful"
 
-# ... (other routes)
 
 if __name__ == '__main__':
     app.run('0.0.0.0' , debug=True)
