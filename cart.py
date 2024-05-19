7fc203b29b68c1d1167a92bd9b955a92d6dc1c73
CHAN125 <buhuibuhui0.0@gmail.com>
2024-05-19 18:07:01 +0800

    added restaurant html, delete useless file

--- cart.py
+++ cart.py
@@ -1,6 +1,19 @@
-from flask import Flask, request, render_template,redirect
+from flask import Flask, request, render_template,redirect, url_for
+import mysql.connector
+
+#NOTE: STOP RUNNING PYTHON WHENEVER YOU WANNA ALTER SQL TABLES
+db = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    passwd='Chan@125',
+    database='customer'
+)
+
+cursor = db.cursor()
 
 app = Flask(__name__)
+app.secret_key = "oisdsaoidiosad"
+
 
 @app.route('/', methods=['GET']) 
 def menu():
@@ -8,21 +21,39 @@ def menu():
         remarks = file.read()
     return render_template('menu.html', remarks=remarks)
 
-@app.route('/', methods=['GET']) 
+@app.route('/home', methods=['GET']) 
 def home():
     return render_template('home.html')
 
 
-@app.route('/add_to_cart', methods=['POST']) 
+@app.route('/add_to_cart', methods=['POST', 'GET']) 
 def add_to_cart():
-    remarks = request.form.get('remarks')
+    if request.method == 'POST':
+        
 
-    with open("remarks.txt", "a") as file:
-        file.write(remarks + "\n")
+        remarks = request.form['remarks']
+        testvalue = "x"
+        print(remarks)
+        sql = "INSERT INTO customer.user_remarks (remarks, username) VALUES (%s, %s)"
+    
+        cursor.execute(sql, (remarks, testvalue))
+        db.commit()
+        
+        # with open("remarks.txt", "a") as file:
+        #     file.write(remarks + "\n")
+        # print(remarks)
 
-    # return render_template('menu.html', remarks=remarks)
-    return "successful"
+        # return render_template('menu.html', remarks=remarks)
+    return render_template("menu.html", remarks=remarks, testvalue=testvalue)
+
+@app.route('/')  # Assuming you want to display remarks on the root route
+def display_remarks():
+  # Connect to database (省略 - omitted for brevity)
+  cursor = db.cursor()
+  cursor.execute("SELECT remarks FROM user_remarks")
+  remarks_data = cursor.fetchall()  # Fetch all remarks as a list of tuples
+  cursor.close()  # Close the cursor
+  return render_template('menu.html', remarks=remarks_data)
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0' , debug=True)
+app.run(debug=True)
